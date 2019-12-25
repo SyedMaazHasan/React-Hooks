47e0b5c83829f2f0edd3ed159916040f04b88bce
Maaz <syed_maaz_hasan@yahoo.com>
2019-12-25 11:09:38 +0530

    added useState Hook

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  const inc = () => {
+    setCount(currentcount => currentcount + 1);
+  };
+  const dec = () => {
+    setCount(currentcount => currentcount - 1);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      count is {count}
+      <button onClick={inc}>increment</button>
+      <button onClick={dec}>increment</button>
     </div>
   );
 }
