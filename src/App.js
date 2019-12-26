7fa31113521ff8f13916936b2ea2698f3225c7a6
Maaz <syed_maaz_hasan@yahoo.com>
2019-12-26 22:54:29 +0530

    added useState & useEffect Hook

--- src/App.js
+++ src/App.js
@@ -1,19 +1,48 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function App() {
   const [count, setCount] = useState(0);
+  const [score, setScore] = useState(0);
 
-  const inc = () => {
+  const incCount = () => {
     setCount(currentcount => currentcount + 1);
   };
-  const dec = () => {
+  const decCount = () => {
     setCount(currentcount => currentcount - 1);
   };
+  const incScore = () => {
+    setScore(currentscore => currentscore + 1);
+  };
+
+  //mounting phase (once) + updation phase (unlimited times)
+  //after every render.... called once in mounting phase + every render in updation phase
+  // useEffect(() => {
+  //   setCount(currentcount => currentcount + 1);
+  // });
+
+  //act as component did mount
+  // //only in mounting phase
+  // useEffect(() => {
+  //   setCount(currentcount => currentcount + 1);
+  // }, []);
+
+  //mounting phase (once) + updation phase(only when second argument is changed)
+  //only in mounting phase and when second argument is changed then it will run it
+  useEffect(() => {
+    setCount(currentcount => currentcount + 1);
+  }, [score]);
+
   return (
     <div>
-      count is {count}
-      <button onClick={inc}>increment</button>
-      <button onClick={dec}>increment</button>
+      <h1>count is {count}</h1>
+      <h1>score is {score}</h1>
+      <button onClick={incCount}>increment counter</button>
+      <button onClick={decCount}>decrement counter</button>
+      <button onClick={incScore}>
+        increment the score first.... and in useEffect, we have a logic which
+        will increment count if score is changed... so both score and count will
+        be incremented
+      </button>
     </div>
   );
 }
